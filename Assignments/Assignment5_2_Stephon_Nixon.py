11437b0750daac56644c519b942b05f8ca6aaaee
Stephon94 <micronixon30@gmail.com>
2016-05-31 18:14:56 -0400

    Assignment 5_2

--- Assignments/Assignment5_2_Stephon_Nixon.py
+++ Assignments/Assignment5_2_Stephon_Nixon.py
@@ -1,24 +1,11 @@
-smallest = None
-largest = None
-
-
-while True:
-	num = raw_input("Enter a number: ")
-	if num == "done": break
+num_list = list()
+while True: 
+	numbers = raw_input('Enter a number: ')
+	if numbers == "done": break
 
 	try:
-		int(num)
-	except ValueError:
-		print "Please enter a number and not a word, Try again" 
+		num_list.append(int(numbers))
+	except:
+		print "Please enter a number and not a word, Try again"
 
-	if num > largest:
-		largest = num
-		
-	if smallest == None:
-		smallest = num
-	
-	if num < smallest:
-		smallest = num
-	
-		
-print "The largest number is ", largest, " & the smallest number is ", smallest
+print "The largest number is ", max(num_list), "& the smallest number is ", min(num_list)
